2d71847cb3957cedb7c88ef0869999b3b1f58ab3
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 16:12:40 +0530

    feat: nav02

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <Nav01 />
+  <Nav02 />
 </template>
 
 <script setup>
-import Nav01 from "./components/00-nav/Nav01.vue";
+import Nav02 from "./components/00-nav/Nav02.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
---
+++ src/components/00-nav/Nav02.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="text-gray-700 bg-white border-t border-b body-font">
+    <div
+      class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row"
+    >
+      <a
+        class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0"
+      >
+        <img src="../../assets/badges/WhitePink.svg" alt="" />
+      </a>
+      <nav class="flex flex-wrap items-center justify-center text-base">
+        <a
+          href="#"
+          class="mr-5 text-sm font-semibold text-gray-600 lg:ml-24 hover:text-gray-800"
+          >Pricing</a
+        >
+        <a
+          href="#"
+          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+          >Contact</a
+        >
+        <a
+          href="#"
+          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+          >Services</a
+        >
+        <a
+          href="#"
+          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+          >Now</a
+        >
+      </nav>
+      <div class="flex ml-auto">
+        <button
+          class="inline-flex px-8 py-3 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 focus:ring focus:outline-none"
+        >
+          Button
+        </button>
+        <button
+          class="items-center px-8 py-3 mt-4 ml-5 font-semibold text-black transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-black0 hover:bg-black hover:text-white focus:ring focus:outline-none"
+        >
+          Button
+          <svg
+            class="hidden w-4 h-4 ml-1 lg:block"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
