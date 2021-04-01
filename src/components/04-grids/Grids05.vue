9df8b6a15528165a24080b1e34906183d92f7a80
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 18:42:37 +0530

    feat: grids

---
+++ src/components/04-grids/Grids05.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="text-gray-700 body-font">
+    <div class="container px-8 pt-48 mx-auto lg:px-4">
+      <div class="flex flex-wrap text-left">
+        <div class="px-8 py-6 lg:w-1/4 md:w-full">
+          <h2
+            class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"
+          >
+            Information 1
+          </h2>
+          <p class="mb-4 text-base leading-relaxed">
+            Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+            hexagon disrupt edison bulbche.
+          </p>
+        </div>
+        <div class="px-8 py-6 lg:w-1/4 md:w-full">
+          <div class="flex items-center mb-3">
+            <h2
+              class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"
+            >
+              Information 2
+            </h2>
+          </div>
+          <p class="mb-4 text-base leading-relaxed">
+            Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+            hexagon disrupt edison bulbche.
+          </p>
+        </div>
+        <div class="px-8 py-6 lg:w-1/4 md:w-full">
+          <div class="flex items-center mb-3">
+            <h2
+              class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"
+            >
+              Information 3
+            </h2>
+          </div>
+          <p class="mb-4 text-base leading-relaxed">
+            Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+            hexagon disrupt edison bulbche.
+          </p>
+        </div>
+        <div class="px-8 py-6 lg:w-1/4 md:w-full">
+          <div class="flex items-center mb-3">
+            <h2
+              class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"
+            >
+              Information 4
+            </h2>
+          </div>
+          <p class="mb-4 text-base leading-relaxed">
+            Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+            hexagon disrupt edison bulbche.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
