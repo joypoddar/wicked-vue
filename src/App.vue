5c7212600a765e9d37c54b28b6d79aab9ac13591
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 16:17:55 +0530

    feat: nav04

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <Nav03 />
+  <Nav04 />
 </template>
 
 <script setup>
-import Nav03 from "./components/00-nav/Nav03.vue";
+import Nav04 from "./components/00-nav/Nav04.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
