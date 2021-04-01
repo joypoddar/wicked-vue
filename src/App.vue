8d84cc5b159e77519153931e7bf5cfdbdf824926
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 17:22:16 +0530

    feat: right header 08

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <RightHeader07 />
+  <RightHeader08 />
 </template>
 
 <script setup>
-import RightHeader07 from "./components/01-right-header/RightHeader07.vue";
+import RightHeader08 from "./components/01-right-header/RightHeader08.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
