ea27fa7ff7509f2ddd193008ded5aeefd7db16ee
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 16:33:00 +0530

    feat: nav08

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <Nav07 />
+  <Nav08 />
 </template>
 
 <script setup>
-import Nav07 from "./components/00-nav/Nav07.vue";
+import Nav08 from "./components/00-nav/Nav08.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
