f2590d8b825f78f4a3cf786c22fa56e647f959a4
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 16:25:47 +0530

    add placeholder component

--- src/components/00-nav/Nav03.vue
+++ src/components/00-nav/Nav03.vue
@@ -1,40 +1,52 @@
 <template>
-  <header class="text-gray-700 bg-white border-t border-b body-font">
+  <div>
+    <header class="text-gray-700 bg-white border-t border-b body-font">
+      <div
+        class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row"
+      >
+        <nav class="flex flex-wrap items-center justify-center text-base">
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Pricing</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Contact</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Services</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Now</a
+          >
+        </nav>
+        <a
+          class="flex items-center w-40 mx-auto mb-4 font-medium text-gray-900 title-font md:mb-0"
+        >
+          <img src="../../assets/badges/WhitePink.svg" alt="" />
+        </a>
+        <button
+          class="items-center px-8 py-3 mt-4 font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:ml-auto lg:mt-px hover:border-blue-700 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
+        >
+          Button
+        </button>
+      </div>
+    </header>
+
     <div
-      class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row"
+      class="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl"
     >
-      <nav class="flex flex-wrap items-center justify-center text-base">
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Pricing</a
-        >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Contact</a
-        >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Services</a
-        >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Now</a
-        >
-      </nav>
-      <a
-        class="flex items-center w-40 mx-auto mb-4 font-medium text-gray-900 title-font md:mb-0"
+      <p
+        class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
       >
-        <img src="../../assets/badges/WhitePink.svg" alt="" />
-      </a>
-      <button
-        class="items-center px-8 py-3 mt-4 font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:ml-auto lg:mt-px hover:border-blue-700 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
-      >
-        Button
-      </button>
+        -- Content goes here --
+      </p>
     </div>
-  </header>
+  </div>
 </template>
--- src/components/00-nav/Nav04.vue
+++ src/components/00-nav/Nav04.vue
@@ -1,42 +1,54 @@
 <template>
-  <header class="text-gray-700 bg-white border-t border-b body-font">
+  <div>
+    <header class="text-gray-700 bg-white border-t border-b body-font">
+      <div
+        class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row"
+      >
+        <a
+          class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0"
+        >
+          <img src="../../assets/badges/WhitePink.svg" alt="" />
+        </a>
+        <nav
+          class="flex flex-wrap items-center justify-center text-base md:ml-auto"
+        >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Pricing</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Contact</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Services</a
+          >
+          <a
+            href="#"
+            class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
+            >Now</a
+          >
+        </nav>
+        <button
+          class="items-center px-8 py-3 mt-4 font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-800 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
+        >
+          Button
+        </button>
+      </div>
+    </header>
+
     <div
-      class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row"
+      class="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl"
     >
-      <a
-        class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0"
+      <p
+        class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
       >
-        <img src="../../assets/badges/WhitePink.svg" alt="" />
-      </a>
-      <nav
-        class="flex flex-wrap items-center justify-center text-base md:ml-auto"
-      >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Pricing</a
-        >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Contact</a
-        >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Services</a
-        >
-        <a
-          href="#"
-          class="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800"
-          >Now</a
-        >
-      </nav>
-      <button
-        class="items-center px-8 py-3 mt-4 font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-800 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
-      >
-        Button
-      </button>
+        -- Content goes here --
+      </p>
     </div>
-  </header>
+  </div>
 </template>
