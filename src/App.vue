fc68f86fbb95e135c95b4ae082c8996c4979b5bf
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 16:28:17 +0530

    feat: nav05

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <Nav04 />
+  <Nav05 />
 </template>
 
 <script setup>
-import Nav04 from "./components/00-nav/Nav04.vue";
+import Nav05 from "./components/00-nav/Nav05.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
---
+++ src/components/00-nav/Nav05.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header class="text-gray-700 bg-white border-t border-b body-font">
+      <div
+        class="container flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row"
+      >
+        <a
+          class="flex items-center w-40 mb-4 font-medium text-gray-900 title-font md:mb-0"
+        >
+          <img src="../../assets/badges/WhitePink.svg" alt="" />
+        </a>
+        <button
+          class="items-center px-8 py-3 mt-4 ml-auto font-semibold text-blue-700 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:inline-flex lg:mt-px hover:border-blue-800 hover:bg-blue-700 hover:text-white focus:ring focus:outline-none"
+        >
+          Button
+        </button>
+      </div>
+    </header>
+    <div
+      class="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl"
+    >
+      <p
+        class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
+      >
+        -- Content goes here --
+      </p>
+    </div>
+  </div>
+</template>
