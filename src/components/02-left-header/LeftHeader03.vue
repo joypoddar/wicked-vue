c4512b80f7afedcf1b3b2e25131803866172041d
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-01 18:10:35 +0530

    feat: left header

---
+++ src/components/02-left-header/LeftHeader03.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="text-gray-700 body-font">
+    <div
+      class="container flex flex-col items-center px-5 py-16 mx-auto lg:px-20 lg:py-24 md:flex-row"
+    >
+      <div
+        class="flex flex-col items-center w-full pt-0 mb-16 text-left lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0 lg:text-center"
+      >
+        <h1
+          class="mb-8 text-2xl font-bold tracking-tighter text-center text-blue-800 lg:text-left lg:text-5xl title-font"
+        >
+          Medium length display headline.
+        </h1>
+        <p
+          class="mb-8 text-base leading-relaxed text-center text-gray-700 lg:text-left lg:text-1xl"
+        >
+          Deploy your mvp in minutes, not days. WT offers you a a wide selection
+          swapable sections for your landing page.
+        </p>
+        <div class="flex justify-center">
+          <input
+            class="flex-grow w-full px-4 py-2 mb-4 mr-4 text-base text-purple-700 bg-gray-100 border border-gray-400 rounded-lg focus:outline-none focus:border-purple-500 sm:mb-0 focus:bg-white"
+            placeholder="Your Email"
+            type="email"
+          />
+          <button
+            class="flex items-center px-4 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 hover:-translate-y-1 hover:scale-110 focus:shadow-outline focus:outline-none"
+          >
+            Action
+          </button>
+        </div>
+        <p class="w-full mt-2 mb-8 text-sm text-left text-gray-600">
+          I got 99 problems and blocks ain't one.
+        </p>
+      </div>
+      <div class="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
+        <img
+          class="object-cover object-center rounded-lg"
+          alt="hero"
+          src="https://dummyimage.com/720x600/F3F4F7/8693ac"
+        />
+      </div>
+    </div>
+  </section>
+</template>
