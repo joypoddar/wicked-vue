8ef3df02c837276480adc24f2a9a937e821eeb1c
Joy Poddar <joypoddar.sd@gmail.com>
2021-04-02 17:32:12 +0530

    feat: footers

---
+++ src/components/10-footers/Footers06.vue
@@ -0,0 +1,131 @@
+<template>
+  <div
+    class="container w-full p-20 m-4 mx-auto mt-8 text-center bg-white border-2 border-dashed h-72 border-blueGray-300 rounded-xl"
+  >
+    <p
+      class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
+    >
+      -- Content goes here --
+    </p>
+  </div>
+  <footer class="mt-6 text-gray-700 bg-white border-t body-font">
+    <div
+      class="container flex flex-col flex-wrap p-5 py-10 mx-auto lg:px-20 md:items-center lg:items-start md:flex-row md:flex-no-wrap"
+    >
+      <div
+        class="flex flex-wrap flex-grow mx-auto mt-10 -mb-10 text-left md:mt-0"
+      >
+        <div class="w-full lg:w-1/3 md:w-full">
+          <h1
+            class="mb-8 text-2xl font-bold text-center text-blue-800 lg:text-left lg:text-2xl title-font"
+          >
+            A pretty long lenght display headline ready to convert visitors into
+            users.
+          </h1>
+        </div>
+        <div class="w-full text-right lg:w-1/3 md:w-1/2">
+          <h1
+            class="mb-3 text-sm font-semibold tracking-widest text-blue-700 uppercase title-font"
+          >
+            Company
+          </h1>
+          <nav class="mb-10 list-none">
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800">Home</a>
+            </li>
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800">About</a>
+            </li>
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800">Carriers</a>
+            </li>
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800">Pricing</a>
+            </li>
+          </nav>
+        </div>
+        <div class="w-full text-right lg:w-1/3 md:w-1/2">
+          <h1
+            class="mb-3 text-sm font-semibold tracking-widest text-blue-700 uppercase title-font"
+          >
+            Legal
+          </h1>
+          <nav class="mb-10 list-none">
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800"
+                >Privacy Policy</a
+              >
+            </li>
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800"
+                >Terms Of Service</a
+              >
+            </li>
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800"
+                >Trademark Policy</a
+              >
+            </li>
+            <li>
+              <a class="text-sm text-gray-600 hover:text-gray-800"
+                >Inactivity Policy</a
+              >
+            </li>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div class="bg-blueGray-900">
+      <div
+        class="container flex flex-col flex-wrap px-5 py-6 mx-auto lg:px-20 sm:flex-row"
+      >
+        <span class="inline-flex justify-center mt-2 sm:mt-0 sm:justify-start">
+          <a class="text-white hover:text-blue-500">
+            <svg
+              fill="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+              ></path>
+            </svg>
+          </a>
+          <a class="ml-4 text-white hover:text-blue-500">
+            <svg
+              fill="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+              ></path>
+            </svg>
+          </a>
+          <a class="ml-4 text-white hover:text-blue-500">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path
+                d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
+              ></path>
+            </svg>
+          </a>
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/10-footers/Footers07.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="container w-full p-20 m-4 mx-auto mt-10 text-center bg-white border-2 border-dashed h-96 border-blueGray-300 rounded-xl"
+  >
+    <p
+      class="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font"
+    >
+      -- Content goes here --
+    </p>
+  </div>
+  <footer class="mt-32 text-gray-700 bg-white border-t body-font">
+    <div class="bg-blueGray-900">
+      <div
+        class="container flex flex-col flex-wrap px-5 py-6 lg:px-20 sm:flex-row"
+      >
+        <div class="flex flex-wrap items-center justify-center text-base">
+          <p class="mr-5 text-sm text-center text-gray-200">
+            © Your Company — 2020
+          </p>
+          <a
+            href="#"
+            class="justify-center mr-5 text-sm text-center text-blueGray-200 hover:text-blue-700"
+          >
+            Privacy Policy</a
+          >
+          <a
+            href="#"
+            class="justify-center mr-5 text-sm text-center text-blueGray-200 hover:text-blue-700"
+          >
+            Terms Of Service</a
+          >
+        </div>
+        <span
+          class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-end"
+        >
+          <p class="mr-5 text-sm text-center text-gray-200">
+            Working Remote Since 2020
+          </p>
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
